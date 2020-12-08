a1cb045fae29e7bf2979d5658a82917835c823de
akrick <akrick@sina.com>
2020-12-08 16:29:51 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/FecthSinaEmails
+
+go 1.15
+
+require (
+	github.com/emersion/go-imap v1.0.6
+	github.com/emersion/go-message v0.14.0
+	golang.org/x/net v0.0.0-20201207224615-747e23833adb
+	golang.org/x/text v0.3.5-0.20201125200606-c27b9fd57aec
+)
--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	_ "github.com/emersion/go-message/charset"
+	"github.com/emersion/go-message/mail"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"github.com/emersion/go-imap"
-	"github.com/emersion/go-imap/client"
 )
 
+func init()  {
+	log.SetFlags(log.Llongfile|log.LstdFlags)
+}
 var (
 	server, email, password string
 	workernum               int
-	imapClient              *client.Client
-	mailDirs                []string
-	mailSumNums             uint32
 )
 
+func determineEncoding(r io.Reader) encoding.Encoding  {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
 func main() {
 	//获取命令行参数
-	//go run main.go -server imap.vip.sina.com:143 -email dengyun@vip.sina.com -password b54aa91e652a203a
+	//go run main.go -server imap.sina.com:143 -email akrick@sina.com -password 22ef61051bfe5c09
 	flag.StringVar(&server, "server", "", "imap服务地址(包含端口)")
 	flag.StringVar(&email, "email", "", "邮箱名")
 	flag.StringVar(&password, "password", "", "密码")
@@ -44,67 +59,93 @@ func main() {
 		log.Fatal(err)
 	}
 	//创建邮件夹目录
-	os.Mkdir(email, os.ModePerm)
-	mailboxes := make(chan *imap.MailboxInfo, 20)
+	_, err = os.Stat(email)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(email, os.ModePerm)
+		if err != nil{
+			log. Fatal(err)
+		}
+	}
+	// Select INBOX
+	mbox, err := imapClient.Select("INBOX", false)
+	if err != nil {
+		log.Println("here we go")
+		log.Fatal(err)
+	}
+
+	// Get the last message
+	if mbox.Messages == 0 {
+		log.Fatal("No message in mailbox")
+	}
+	seqSet := new(imap.SeqSet)
+	seqSet.AddNum(1, mbox.Messages)
+
+	// Get the whole message body
+	var section imap.BodySectionName
+	items := []imap.FetchItem{section.FetchItem()}
+
+	messages := make(chan *imap.Message, 10)
 	go func() {
-		imapClient.List("", "*", mailboxes)
+		if err := imapClient.Fetch(seqSet, items, messages); err != nil {
+			log.Fatal(err)
+		}
 	}()
-	//列取邮件夹
-	for m := range mailboxes {
-		mailDirs = append(mailDirs, m.Name)
+
+	msg := <-messages
+	if msg == nil {
+		log.Fatal("Server didn't returned message")
 	}
 
-	for _, mailDir := range mailDirs {
-		//选中每个邮件夹
-		mbox, err := imapClient.Select(mailDir, false)
-		if err != nil {
-			log.Fatal(err)
-		}
-		mailDirNums := mbox.Messages
-		log.Printf("%s : %d", mailDir, mailDirNums)
-		fileDir := fmt.Sprintf("%s/%s_%d", email, mailDir, mailDirNums)
-		//创建邮件夹目录
-		os.Mkdir(fileDir, os.ModePerm)
-		mailSumNums += mailDirNums
+	r := msg.GetBody(&section)
+	if r == nil {
+		log.Fatal("Server didn't returned message body")
 	}
-	log.Printf("总邮件数 : %d", mailSumNums)
-	for _, mailDir := range mailDirs {
-		//选中每个邮件夹
-		mbox, err := imapClient.Select(mailDir, false)
-		if err != nil {
+
+	fmt.Println(r)
+	os.Exit(0)
+
+	// Create a new mail reader
+	e := determineEncoding(r)
+	//转为UTF8格式读
+	utf8Reader := transform.NewReader(r, e.NewDecoder())
+	ur, err := mail.CreateReader(utf8Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Print some info about the message
+	//header := ur.Header
+	//if date, err := header.Date(); err == nil {
+	//	log.Println("Date:", date)
+	//}
+	//if from, err := header.AddressList("From"); err == nil {
+	//	log.Println("From:", from)
+	//}
+	//if to, err := header.AddressList("Reply to"); err == nil {
+	//	log.Println("Reply to:", to)
+	//}
+	//if subject, err := header.Subject(); err == nil {
+	//	log.Println("Subject:", subject)
+	//}
+
+	// Process each message's part
+	for {
+		p, err := ur.NextPart()
+		if err == io.EOF {
+			log.Println("done")
+			break
+		} else if err != nil {
 			log.Fatal(err)
 		}
-		//循环该邮件夹中的邮件
-		seqset := new(imap.SeqSet)
-		seqset.AddRange(1, mbox.Messages)
-		section := &imap.BodySectionName{}
-		items := []imap.FetchItem{section.FetchItem()}
-
-		messages := make(chan *imap.Message, mbox.Messages)
-		go func() {
-			imapClient.Fetch(seqset, items, messages)
-		}()
-
-		for msg := range messages {
-			mailFile := fmt.Sprintf("%s/%s_%d/%d.eml", email, mailDir, mbox.Messages, msg.SeqNum)
-			r := msg.GetBody(section)
-			if r == nil {
-				log.Printf("%s-%dServer didn't returned message body", mailDir, msg.SeqNum)
-			}
-			if r != nil {
-				body, err := ioutil.ReadAll(r)
-				if err != nil {
-					log.Printf("%s:%d ioutil.ReadAll error", mailDir, msg.SeqNum)
-				}
-
-				file6, err := os.OpenFile(mailFile, os.O_RDWR|os.O_CREATE, 0766)
-				if err != nil {
-					log.Printf("%s:%d os.OpenFile error %s", mailDir, msg.SeqNum, mailFile)
-				}
-				file6.Write(body)
-				file6.Close()
-				log.Printf("%s :第 %d ", mailDir, msg.SeqNum)
-			}
+		switch h := p.Header.(type) {
+			case *mail.InlineHeader:
+				// This is the message's text (can be plain-text or HTML)
+				b, _ := ioutil.ReadAll(p.Body)
+				log.Println("Body:", string(b))
+			case *mail.AttachmentHeader:
+				// This is an attachment
+				filename, _ := h.Filename()
+				log.Println("Got attachment:", filename)
 		}
 	}
 }
