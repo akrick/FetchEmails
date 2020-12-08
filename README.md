5ea3a19648ffbf43b22bd5955671189f04f97874
akrick <akrick@sina.com>
2020-12-08 21:27:08 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FecthSinaEmails
-Fecth Sina Emails
+Fecth Emails and filter the email address out from the content
+
+command, as bellow:
+```
+go run main.go -server imap.sina.com:143 -email akrick@sina.com -password 22ef61051bfe5c09
+```
--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 	"github.com/emersion/go-message/mail"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 )
 
 func init()  {
@@ -25,17 +25,28 @@ var (
 	workernum               int
 )
 
-func determineEncoding(r io.Reader) encoding.Encoding  {
-	bytes, err := bufio.NewReader(r).Peek(1024)
+func PutData(email string) (err error) {
+
+	var f *os.File
+	//write csv record
+	csvFile := path + "/data.csv"
+	f, err = os.OpenFile(csvFile, os.O_APPEND|os.O_CREATE, 0755)
+
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
+	_, err = f.Write([]byte(email+"\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+	return
 }
+
 func main() {
 	//获取命令行参数
-	//go run main.go -server imap.sina.com:143 -email akrick@sina.com -password 22ef61051bfe5c09
 	flag.StringVar(&server, "server", "", "imap服务地址(包含端口)")
 	flag.StringVar(&email, "email", "", "邮箱名")
 	flag.StringVar(&password, "password", "", "密码")
@@ -43,7 +54,7 @@ func main() {
 	flag.Parse()
 	if flag.NFlag() < 3 {
 		flag.PrintDefaults()
-		os.Exit(1)
+		log.Fatal("参数缺失")
 	}
 
 	if server == "" || email == "" || password == "" || workernum == 0 {
@@ -69,83 +80,68 @@ func main() {
 	// Select INBOX
 	mbox, err := imapClient.Select("INBOX", false)
 	if err != nil {
-		log.Println("here we go")
 		log.Fatal(err)
 	}
-
 	// Get the last message
 	if mbox.Messages == 0 {
 		log.Fatal("No message in mailbox")
 	}
+
 	seqSet := new(imap.SeqSet)
-	seqSet.AddNum(1, mbox.Messages)
+	seqSet.AddRange(1, mbox.Messages)
 
 	// Get the whole message body
 	var section imap.BodySectionName
 	items := []imap.FetchItem{section.FetchItem()}
-
 	messages := make(chan *imap.Message, 10)
 	go func() {
 		if err := imapClient.Fetch(seqSet, items, messages); err != nil {
 			log.Fatal(err)
 		}
 	}()
-
-	msg := <-messages
-	if msg == nil {
-		log.Fatal("Server didn't returned message")
-	}
-
-	r := msg.GetBody(&section)
-	if r == nil {
-		log.Fatal("Server didn't returned message body")
-	}
-
-	fmt.Println(r)
-	os.Exit(0)
-
-	// Create a new mail reader
-	e := determineEncoding(r)
-	//转为UTF8格式读
-	utf8Reader := transform.NewReader(r, e.NewDecoder())
-	ur, err := mail.CreateReader(utf8Reader)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//Print some info about the message
-	//header := ur.Header
-	//if date, err := header.Date(); err == nil {
-	//	log.Println("Date:", date)
-	//}
-	//if from, err := header.AddressList("From"); err == nil {
-	//	log.Println("From:", from)
-	//}
-	//if to, err := header.AddressList("Reply to"); err == nil {
-	//	log.Println("Reply to:", to)
-	//}
-	//if subject, err := header.Subject(); err == nil {
-	//	log.Println("Subject:", subject)
-	//}
-
-	// Process each message's part
-	for {
-		p, err := ur.NextPart()
-		if err == io.EOF {
-			log.Println("done")
+	var i uint32
+	i = 1
+	for  {
+		if i == mbox.Messages {
 			break
-		} else if err != nil {
+		}
+		msg := <-messages
+		if msg == nil {
+			log.Fatal("Server didn't returned message")
+		}
+
+		r := msg.GetBody(&section)
+		if r == nil {
+			log.Fatal("Server didn't returned message body")
+		}
+
+		// Create a new mail reader
+		mr, err := mail.CreateReader(r)
+		if err != nil {
 			log.Fatal(err)
 		}
-		switch h := p.Header.(type) {
-			case *mail.InlineHeader:
-				// This is the message's text (can be plain-text or HTML)
-				b, _ := ioutil.ReadAll(p.Body)
-				log.Println("Body:", string(b))
-			case *mail.AttachmentHeader:
-				// This is an attachment
-				filename, _ := h.Filename()
-				log.Println("Got attachment:", filename)
+
+		// Process each message's part
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			b, _ := ioutil.ReadAll(p.Body)
+			reEmail := `[\w\.]+@\w+\.[a-z]{2,3}(\.[a-z]{2,3})?`
+			re := regexp.MustCompile(reEmail)
+			//-1 表示匹配所有  如果输入5  表示只匹配5个
+			email := re.FindStringSubmatch(string(b))
+			if len(email) > 0 {
+				err = PutData(email[0])
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			fmt.Println(email)
 		}
+		i++
 	}
 }
